539296c5b1977eddd60d2d39e4490615a8984fef
Andrew <andrey.aleksandrychev2@mail.ru>
2021-04-10 09:19:39 +0300

    Second

--- server.js
+++ server.js
@@ -40,6 +40,54 @@ app.get('/updateData', (req, res) => {
     )
 })
 
+app.get('/page1', (req, res) => {
+    res.render('page1')
+})
+
+app.get('/page2', (req, res) => {
+    res.render('page2')
+})
+
+app.get('/page3', (req, res) => {
+    res.render('page3')
+})
+
+app.get('/page4', (req, res) => {
+    res.render('page4')
+})
+
+app.get('/page5', (req, res) => {
+    res.render('page5')
+})
+
+app.get('/page6', (req, res) => {
+    res.render('page6')
+})
+
+app.get('/page7', (req, res) => {
+    res.render('page7')
+})
+
+app.get('/page8', (req, res) => {
+    res.render('page8')
+})
+
+app.get('/page9', (req, res) => {
+    res.render('page9')
+})
+
+app.get('/page10', (req, res) => {
+    res.render('page10')
+})
+
+app.get('/page11', (req, res) => {
+    res.render('page11')
+})
+
+app.get('/page12', (req, res) => {
+    res.render('page12')
+})
+
 app.get('*', function(req, res){
     res.render('404');
 });
